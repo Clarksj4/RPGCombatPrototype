4cff71b80a1dccd9028f9596b91cb728af8c1090
Stephen <clarksj4@student.op.ac.nz>
2020-08-28 07:57:23 +1200

    Add simple health bar to pawns

---
+++ Assets/Scripts/UI/PawnUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PawnUI : MonoBehaviour
+{
+    [SerializeField]
+    private Image healthBar;
+    private Pawn pawn;
+
+    private void Awake()
+    {
+        pawn = GetComponentInParent<Pawn>();
+        pawn.OnHealthChanged += HandleOnPawnHealthChanged;
+    }
+
+    private void HandleOnPawnHealthChanged(int change)
+    {
+        float fill = (float)pawn.Health / pawn.MaxHealth;
+        healthBar.fillAmount = fill;
+    }
+}
