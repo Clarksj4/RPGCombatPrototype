162200137d4b0f6541cf580bc581cebf2329b869
stephenclark <stephen@runawayplay.com>
2020-08-29 12:40:53 +1200

    Add sprite retrieval objects

---
+++ Assets/Scripts/Sprites/SpriteCatalogue.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpriteCatalogue", menuName = "ScriptableObjects/SpriteCatalogue", order = 1)]
+public class SpriteCatalogue : ScriptableObject
+{
+    public Sprite[] Sprites;
+}
---
+++ Assets/Scripts/Sprites/SpriteManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class SpriteManager : Singleton<SpriteManager>
+{
+    private SpriteCatalogue spriteCatalogue;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        spriteCatalogue = Resources.Load<SpriteCatalogue>("SpriteCatalogue");
+    }
+
+    public Sprite GetSpriteByName(string name)
+    {
+        return spriteCatalogue.Sprites.FirstOrDefault(s => s.name == name);
+    }
+}
