78ba057955aa3ac7e2b497435f821ada5b6c5ee5
stephenclark <stephen@runawayplay.com>
2020-08-29 20:22:55 +1200

    Actor stats now settable in editor

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -1,44 +1,52 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Actor : Pawn, ITurnBased, IAttacker
 {
-    /// <summary>
-    /// Gets this actor's name.
-    /// </summary>
-    public string Name { get; set; }
+    [SerializeField] private float priority = 90f;
+    [SerializeField] private float attack = 10f;
+    [SerializeField] private float accuracy = 1f;
+    [SerializeField] private int movement = 3;
+    [SerializeField] private int attackRange = 1;
+    [SerializeField] private List<string> actions = new List<string>() { "Move", "Attack", "Push" };
+
     /// <summary>
     /// Gets this actors priority in the turn order. Determines
     /// how frequently it gets to act.
     /// </summary>
-    public float Priority { get; set; }
+    public float Priority { get { return priority; } }
     /// <summary>
     /// Gets whether this actor is currently able to take actions.
     /// </summary>
-    public bool Incapacitated { get; private set; }
+    public bool Incapacitated { get; set; }
     /// <summary>
     /// Gets the amount of damage this actor can do with each
     /// attack.
     /// </summary>
-    public float Attack { get; private set; } = 10f;
+    public float Attack { get { return attack; } }
     /// <summary>
     /// Gets how accurate this actor with their actions.
     /// </summary>
-    public float Accuracy { get; private set; } = 1f;
+    public float Accuracy { get { return accuracy; } }
     /// <summary>
     /// Gets how far this actor can move in their turn.
     /// </summary>
-    public int Movement { get; private set; } = 3;
+    public int Movement { get { return movement; } }
     /// <summary>
     /// Gets the range on this actors attacks.
     /// </summary>
-    public int AttackRange { get; private set; } = 1;
+    public int AttackRange { get { return attackRange; } }
     /// <summary>
     /// Gets the actions available to this actor.
     /// </summary>
-    public List<string> Actions { get; private set; } = new List<string>()
+    public List<string> Actions { get { return actions; } }
+
+    protected override void Awake()
     {
-        "Move", "Attack", "Push"
-    };
+        base.Awake();
+
+        TurnManager.Instance.Add(this);
+    }
     /// <summary>
     /// Ends this actors turn.
     /// </summary>
