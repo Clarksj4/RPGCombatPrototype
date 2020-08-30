045389f7af2e84e758e89754ffaf2b347e9146db
stephenclark <stephen@runawayplay.com>
2020-08-30 14:23:13 +1200

    Centre grid at its world position

--- Assets/Scripts/GridBased/BattleMap.cs
+++ Assets/Scripts/GridBased/BattleMap.cs
@@ -21,14 +21,14 @@ public class BattleMap : MonoBehaviour
     {
         for (int x = 0; x <= grid.NCells.x; x++)
         {
-            Vector2 from = new Vector2(x * grid.CellSize.x, 0);
+            Vector2 from = new Vector2(grid.Bounds.min.x + (x * grid.CellSize.x), grid.Bounds.min.y);
             Vector2 to = from + (Vector2.up * grid.CellSize * grid.NCells.y);
             Gizmos.DrawLine(from, to);
         }
 
         for (int y = 0; y <= grid.NCells.y; y++)
         {
-            Vector2 from = new Vector2(0, y * grid.CellSize.y);
+            Vector2 from = new Vector2(grid.Bounds.min.x, grid.Bounds.min.y + (y * grid.CellSize.y));
             Vector2 to = from + (Vector2.right * grid.CellSize * grid.NCells.x);
             Gizmos.DrawLine(from, to);
         }
--- Assets/Scripts/GridBased/Grid.cs
+++ Assets/Scripts/GridBased/Grid.cs
@@ -7,6 +7,7 @@ public class Grid
     public Vector2Int NCells { get { return nCells; } }
     public Vector2 CellSize { get { return cellSize; } }
     public Vector2 WorldPosition { get { return worldPosition; } }
+    public Bounds Bounds { get { return new Bounds(WorldPosition, nCells * cellSize); } }
 
     [SerializeField]
     private Vector2Int nCells;
@@ -24,12 +25,12 @@ public class Grid
 
     public Vector2 CoordinateToWorldPosition(Vector2Int coordinate)
     {
-        return WorldPosition + (coordinate * CellSize) + (CellSize * 0.5f);
+        return (Vector2)Bounds.min + (coordinate * CellSize) + (CellSize * 0.5f);
     }
 
     public bool WorldPositionToCoordinate(Vector2 worldPosition, out Vector2Int coordinate)
     {
-        Vector2 delta = worldPosition - WorldPosition;
+        Vector2 delta = worldPosition - (Vector2)Bounds.min;
         Vector2 unboundedCoordinate = delta / CellSize;
         Vector2Int roundedUnboundedCoordinate = new Vector2Int((int)unboundedCoordinate.x, (int)unboundedCoordinate.y);
 
