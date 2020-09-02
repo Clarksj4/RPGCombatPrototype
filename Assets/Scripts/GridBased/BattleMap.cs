661f9a78774f69c92b23a65e8a59044fc33df5e4
stephenclark <stephen@runawayplay.com>
2020-09-02 21:00:31 +1200

    Add beginnings of battle map renderer

--- Assets/Scripts/GridBased/BattleMap.cs
+++ Assets/Scripts/GridBased/BattleMap.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class BattleMap : MonoBehaviour
 {
+    public Grid Grid { get { return grid; } }
     [SerializeField]
     private Grid grid;
 
@@ -15,23 +16,6 @@ public class BattleMap : MonoBehaviour
     private void Awake()
     {
         pawns = GetComponentsInChildren<Pawn>();
-    }
-
-    private void OnDrawGizmos()
-    {
-        for (int x = 0; x <= grid.NCells.x; x++)
-        {
-            Vector2 from = new Vector2(grid.Bounds.min.x + (x * grid.CellSize.x), grid.Bounds.min.y);
-            Vector2 to = from + (Vector2.up * grid.CellSize * grid.NCells.y);
-            Gizmos.DrawLine(from, to);
-        }
-
-        for (int y = 0; y <= grid.NCells.y; y++)
-        {
-            Vector2 from = new Vector2(grid.Bounds.min.x, grid.Bounds.min.y + (y * grid.CellSize.y));
-            Vector2 to = from + (Vector2.right * grid.CellSize * grid.NCells.x);
-            Gizmos.DrawLine(from, to);
-        }
     }
 
     private void OnMouseDown()
---
+++ Assets/Scripts/GridBased/BattleMapRenderer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BattleMapRenderer : MonoBehaviour
+{
+    private BattleMap map;
+
+    private void Awake()
+    {
+        map = GetComponentInParent<BattleMap>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            for (int x = 0; x <= map.Grid.NCells.x; x++)
+            {
+                Vector2 from = new Vector2(map.Grid.Bounds.min.x + (x * map.Grid.CellSize.x), map.Grid.Bounds.min.y);
+                Vector2 to = from + (Vector2.up * map.Grid.CellSize * map.Grid.NCells.y);
+                Gizmos.DrawLine(from, to);
+            }
+
+            for (int y = 0; y <= map.Grid.NCells.y; y++)
+            {
+                Vector2 from = new Vector2(map.Grid.Bounds.min.x, map.Grid.Bounds.min.y + (y * map.Grid.CellSize.y));
+                Vector2 to = from + (Vector2.right * map.Grid.CellSize * map.Grid.NCells.x);
+                Gizmos.DrawLine(from, to);
+            }
+        }
+    }
+}
