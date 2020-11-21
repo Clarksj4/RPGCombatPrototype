5ad16e886e11d01a434ea2f5f93b4d07596d598b
stephenclark <stephen@runawayplay.com>
2020-11-21 16:08:40 +1300

    Add frames and highlighting to actors

---
+++ Assets/Scripts/ActorAppearance.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Actor))]
+    public class ActorAppearance : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer frameImage;
+        [SerializeField]
+        private SpriteRenderer actorImage;
+
+        private Actor actor;
+
+        private void Awake()
+        {
+            actor = GetComponent<Actor>();
+
+            TurnManager.Instance.OnTurnStart += HandleOnTurnStart;
+            TurnManager.Instance.OnTurnEnd += HandleOnTurnEnd;
+        }
+
+        private void Start()
+        {
+            RefreshActorImage();
+        }
+
+        private void RefreshActorImage()
+        {
+            actorImage.sprite = SpriteManager.Instance.GetSpriteByName(name);
+        }
+
+        private void HandleOnTurnStart(ITurnBased turnBasedEntity)
+        {
+            if (actor == turnBasedEntity)
+                frameImage.color = Color.green;
+        }
+
+        private void HandleOnTurnEnd(ITurnBased turnBasedEntity)
+        {
+            if (actor == turnBasedEntity)
+                frameImage.color = Color.white;
+        }
+    }
+}
