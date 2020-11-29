5b2aa356bb260e56781c7dd095c7dc58c41daa9f
stephenclark <stephen@runawayplay.com>
2020-11-29 20:44:13 +1300

    Remove redunant event handlers in cancel bar

--- Assets/Scripts/UI/Menus/ActionBars/CancelBar.cs
+++ Assets/Scripts/UI/Menus/ActionBars/CancelBar.cs
@@ -8,27 +8,12 @@ public class CancelBar : Menu
 
         ActionManager.Instance.OnActionSelected += HandleOnActionSelected;
         ActionManager.Instance.OnActionDeselected += HandleOnActionDeslected;
-        ActionManager.Instance.OnTargetSelected += HandleOnTargetSelected;
-        ActionManager.Instance.OnTargetDeselected += HandleOnTargetDeslected;
-    }
-
-    private void HandleOnTargetDeslected(BattleAction obj)
-    {
-        //MenuStack.Instance.Hide();
-    }
-
-    private void HandleOnTargetSelected(BattleAction obj)
-    {
-        if (MenuStack.Instance.Current != this)
-            MenuStack.Instance.Show(this);
     }
 
     public void OnCancelTapped()
     {
         if (ActionManager.Instance.HasAction)
             ActionManager.Instance.ClearSelectedAction();
-        else if (ActionManager.Instance.HasTarget)
-            ActionManager.Instance.ClearSelectedTarget();
     }
 
     private void HandleOnActionSelected(BattleAction obj)
