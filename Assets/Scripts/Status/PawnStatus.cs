f89d7dab2e805027f51737128dccad59bb0170ba
stephenclark <stephen@runawayplay.com>
2021-01-23 14:57:16 +1300

    PawnStatus now serializable

--- Assets/Scripts/Status/PawnStatus.cs
+++ Assets/Scripts/Status/PawnStatus.cs
@@ -2,18 +2,39 @@
 using System.Collections;
 using System;
 
+[Serializable]
 public abstract class PawnStatus
 {
+    /// <summary>
+    /// Occurs when this status expires.
+    /// </summary>
     public event Action<PawnStatus> OnStatusExpired;
-
+    /// <summary>
+    /// Gets the pawn this status is applied to.
+    /// </summary>
     public Pawn Pawn { get; set; }
-    public int Duration { get; protected set; }
-
+    /// <summary>
+    /// Gets or sets the remaining duration that this
+    /// status will afflicted the targeted pawn.
+    /// </summary>
+    public int Duration 
+    { 
+        get { return duration; }
+        set { duration = value; }
+    }
+    [SerializeField]
+    private int duration;
+    /// <summary>
+    /// Creates a new status that lasts for the given
+    /// number of turns.
+    /// </summary>
     public PawnStatus(int duration)
     {
         Duration = duration;
     }
-
+    /// <summary>
+    /// Applies this status.
+    /// </summary>
     public void Apply()
     {
         TurnManager.Instance.OnTurnStart += HandleOnTurnBegin;
@@ -21,15 +42,25 @@ public abstract class PawnStatus
 
         OnApplication();
     }
-
+    /// <summary>
+    /// Checks for and handle interactions between statuses.
+    /// </summary>
     public virtual bool Collate(PawnStatus other) { return false; }
-
+    /// <summary>
+    /// Occurs when this status is applied to the target pawn.
+    /// </summary>
     protected virtual void OnApplication() { /* Nothing! */ }
-
+    /// <summary>
+    /// Occurs at the beginning of each turn.
+    /// </summary>
     protected virtual void DoEffect() {  /* Nothing! */ }
-
+    /// <summary>
+    /// Occurs when this status expires.
+    /// </summary>
     protected virtual void OnExpired() { /* Nothing! */ }
-
+    /// <summary>
+    /// Removes this status from the afflicted pawn.
+    /// </summary>
     protected void Expire()
     {
         TurnManager.Instance.OnTurnStart -= HandleOnTurnBegin;
