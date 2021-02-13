a786c27ba6bea0e30876928e7df0f73414cc4671
stephenclark <stephen@runawayplay.com>
2021-02-14 09:32:36 +1300

    WIP node displays

---
+++ Assets/Scripts/UI/BattleActionInfo/ActionNodeDisplay/DamageNodeDisplay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class DamageNodeDisplay : ActionNodeDisplay
+{
+    [Header("Components")]
+    private TextMeshProUGUI text;
+
+    public override void Setup(ActionNode node) 
+    {
+        DoDamageNode damageNode = node as DoDamageNode;
+        text.text = damageNode.BaseDamage.ToString();
+    }
+}
