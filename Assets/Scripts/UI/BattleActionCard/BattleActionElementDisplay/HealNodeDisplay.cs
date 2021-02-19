944b7eef8ba28f98c8febc5b9e0aec79bb8564bb
stephenclark <stephen@runawayplay.com>
2021-02-20 00:34:11 +1300

    More action card WIP

---
+++ Assets/Scripts/UI/BattleActionCard/BattleActionElementDisplay/HealNodeDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using TMPro;
+
+public class HealNodeDisplay : BattleActionElementDisplay
+{
+    [BoxGroup("Components")]
+    [SerializeField]
+    private TextMeshProUGUI amountText;
+
+    public override void Setup(IBattleActionElement element)
+    {
+        HealNode healNode = element as HealNode;
+        amountText.text = healNode.Amount.ToString();
+    }
+}
