2fd1b3d6a7bcbd576192363c52337777a9ac04a0
stephenclark <stephen@runawayplay.com>
2021-02-21 19:08:43 +1300

    Comments

--- Assets/Scripts/UI/Menus/ActionBars/ActionsBar.cs
+++ Assets/Scripts/UI/Menus/ActionBars/ActionsBar.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class ActionsBar : Menu
 {
+    /// <summary>
+    /// Gets the number of actions the currently selected actor has.
+    /// </summary>
     private int ActionCount { get { return ActionManager.Instance.SelectedActor.BattleActions.Count; } }
 
-    [SerializeField]
-    private ActionButton[] actionButtons = null;
+    [Tooltip("All of the buttons for displaying actions.")]
+    [SerializeField] private ActionButton[] actionButtons = null;
 
     protected override void Awake()
     {
@@ -25,10 +26,13 @@ public class ActionsBar : Menu
 
         RefreshButtons();
     }
-
+    
+    /// <summary>
+    /// Update the actions shown on the buttons and their visibility. 
+    /// </summary>
     private void RefreshButtons()
     {
-        // Populate bar actions with actor's attacks
+        // Populate buttons with actor's actions
         for (int i = 0; i < actionButtons.Length; i++)
         {
             ActionButton button = actionButtons[i];
@@ -39,6 +43,7 @@ public class ActionsBar : Menu
 
             if (enabled)
             {
+                // Set the button's associated action.
                 BattleAction action = ActionManager.Instance.SelectedActor.BattleActions[i];
                 button.SetAction(action);
             }
@@ -47,36 +52,38 @@ public class ActionsBar : Menu
 
     public void OnActionTapped(int index)
     {
+        // Set action as the one the actor is going to use.
         ActionManager.Instance.SelectActionByIndex(index);
     }
 
     public void OnEndTurnTapped()
     {
+        // End actor's turn
         ActionManager.Instance.EndSelectedActorTurn();
-    }
-
-    public void OnCancelTapped()
-    {
-        ActionManager.Instance.ClearSelectedAction();
     }
 
     private void HandleOnActorSelected(Pawn actor)
     {
+        // Show action bar for the given actor.
         MenuStack.Instance.Show(this);
     }
 
     private void HandleOnActorDeselected(Pawn actor)
     {
+        // Hide action bar - there's no actor to display actions for.
         MenuStack.Instance.Hide();
     }
 
     private void HandleOnActionStarted(Pawn actor, BattleAction action)
     {
+        // Hide the action bar when the action starts so it's not in the way
         MenuStack.Instance.Hide();
     }
 
     private void HandleOnActionComplete(Pawn actor, BattleAction action)
     {
+        // Refresh the buttons when the action is finished so that
+        // their mana costs etc are updated.
         RefreshButtons();
     }
 }
