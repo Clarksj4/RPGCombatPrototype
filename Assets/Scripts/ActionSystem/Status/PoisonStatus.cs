ddc690f0cc9ff3a083ade0a884d3ea01c6698485
stephenclark <stephen@runawayplay.com>
2021-11-02 22:01:00 +1300

    Define poison damage as a const value because it's uniform across all instances.

--- Assets/Scripts/ActionSystem/Status/PoisonStatus.cs
+++ Assets/Scripts/ActionSystem/Status/PoisonStatus.cs
@@ -3,12 +3,17 @@
 [Serializable]
 public class PoisonStatus : PawnStatus
 {
+    /// <summary>
+    /// All instances of poison deal the same percent max health damage.
+    /// </summary>
+    private const float percentMaxHealthDamagePerTurn = 0.1f;
+
     protected override void DoEffect()
     {
         base.DoEffect();
 
-        // Take 10% max health damage each tick.
-        Pawn.TakeDamage((int)(Pawn.Stats["Health"].Max * 0.15f), false);
+        // Take damage as a percent of max health each tick.
+        Pawn.TakeDamage((int)(Pawn.Stats["Health"].Max * percentMaxHealthDamagePerTurn), false);
     }
 
     public override bool Collate(PawnStatus other)
